bc466f46c14a6620e7befacf1962134e1b1a84b7
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-11-10 09:57:22 +0100

    implement phone exploration 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+
+import * as Sentry from "@sentry/react";
 
 function App() {
-
   return (
     <main className="bg-black">
       <NavBar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://c0fb70a0403ea305b673d689c83ea9d3@o4508215536713728.ingest.de.sentry.io/4508272921673808",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
