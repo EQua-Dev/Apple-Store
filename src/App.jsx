f217d78f6708928067138aadffd4a6efd478c766
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-11-10 12:10:47 +0100

    implement footer and done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Model from "./components/Model";
 
 import * as Sentry from "@sentry/react";
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 }
